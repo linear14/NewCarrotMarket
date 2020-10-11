2fb068de1ad9aef5157fff874d48ea4374123844
linear14 <linear14@naver.com>
2020-10-11 20:25:13 +0900

    add WriteUsedItemActivity (UI) #temp
    +) bottomsheet selector

---
+++ app/app/src/main/java/com/dongldh/carrot/ui/WriteUsedItemActivity.kt
@@ -0,0 +1,12 @@
+package com.dongldh.carrot.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongldh.carrot.R
+
+class WriteUsedItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_used_item)
+    }
+}
