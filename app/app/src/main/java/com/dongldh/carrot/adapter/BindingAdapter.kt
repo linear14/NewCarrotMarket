fe1f91442bff37d7ca7720c34fa849be4159764f
LeeDongHyun <linear14@naver.com>
2020-11-09 23:35:31 +0900

    add: show time passed / price by using BindingAdapter
    - Time Passed
    - Price
    for usedItem

--- app/app/src/main/java/com/dongldh/carrot/adapter/BindingAdapter.kt
+++ app/app/src/main/java/com/dongldh/carrot/adapter/BindingAdapter.kt
@@ -1,7 +1,12 @@
 package com.dongldh.carrot.adapter
 
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.dongldh.carrot.R
+import com.dongldh.carrot.data.NO_PRICE
+import com.dongldh.carrot.util.App
+import java.util.*
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, isGone: Boolean) {
@@ -9,5 +14,26 @@ fun bindIsGone(view: View, isGone: Boolean) {
         View.GONE
     } else {
         View.VISIBLE
+    }
+}
+
+@BindingAdapter(value = ["bind:timeStamp"])
+fun bindGetTimeUpdated(view: TextView, timeStamp: Long) {
+    val gap = Calendar.getInstance().timeInMillis - timeStamp
+    when(gap) {
+        in 0 until (60 * 1000) -> view.text = "방금 전"
+        in (60 * 1000) until (3600 * 1000) -> view.text = "${gap / (60 * 1000)}분 전"
+        in (3600 * 1000) until (3600 * 24 * 1000) -> view.text = "${gap / (3600 * 1000)}시간 전"
+        in (3600 * 24 * 1000) until (3600 * 24 * 14 * 1000) -> view.text = "${gap / (3600 * 24 * 1000)}일 전"
+        else -> view.text = "오래 전"
+    }
+}
+
+@BindingAdapter("price")
+fun bindPrice(view: TextView, price: Int) {
+    if(price != NO_PRICE) {
+        view.text = "${price}원"
+    } else {
+        view.text = ""
     }
 }
