6e4f5169007123dc7ced9c8c0f90406433dc877d
LeeDongHyun <linear14@naver.com>
2020-11-25 22:37:17 +0900

    add layout: activity_item_detail.xml

---
+++ app/app/src/main/java/com/dongldh/carrot/ui/ItemDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dongldh.carrot.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongldh.carrot.R
+
+class ItemDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+    }
+}
